2518d904c2544a5724ea7794a0912a2d05a2bf54
Ryan Sweeney <sween119@hotmail.com>
2020-07-04 11:40:48 -0400

    Added an extension for Vector clamping

---
+++ Extensions/MathExtensions.cs
@@ -0,0 +1,20 @@
+using System;
+using Microsoft.Xna.Framework;
+
+namespace SweenGame.Extensions
+{
+    public static class MathExtensions
+    {
+        public static void ClampToNearest(this Vector2 vector, int nearest)
+        {
+            var xMin = (int)Math.Floor(vector.X / nearest) * nearest;
+            var yMin = (int)Math.Floor(vector.Y / nearest) * nearest;
+
+            var xMax = (int)Math.Ceiling(vector.X / nearest) * nearest;
+            var yMax = (int)Math.Ceiling(vector.Y / nearest) * nearest;
+
+
+            vector = new Vector2(Math.Clamp(vector.X, xMin, xMax), Math.Clamp(vector.Y, yMin, yMax));
+        }
+    }
+}
