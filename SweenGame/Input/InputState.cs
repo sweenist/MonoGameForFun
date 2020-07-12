91ece80a1e789ca8b62861d9a03c0b96741d220a
Ryan Sweeney <sween119@hotmail.com>
2020-07-12 10:01:58 -0400

    Added InputState for input handling

---
+++ SweenGame/Input/InputState.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Xna.Framework.Input;
+
+namespace SweenGame.Input
+{
+    public class InputState
+    {
+        public KeyboardState CurrentKeyBoardState;
+        public KeyboardState LastKeyBoardState;
+
+        public bool MenuUp => IsNewKeyPress(Keys.Up);
+
+        public bool MenuDown => IsNewKeyPress(Keys.Down);
+
+        public bool MenuSelect => IsNewKeyPress(Keys.Space) || IsNewKeyPress(Keys.Enter);
+        public bool MenuCancel => IsNewKeyPress(Keys.Escape);
+        public bool PauseGame => IsNewKeyPress(Keys.Escape);
+
+        public void Update()
+        {
+            LastKeyBoardState = CurrentKeyBoardState;
+            CurrentKeyBoardState = Keyboard.GetState();
+        }
+
+        public bool IsNewKeyPress(Keys key)
+        {
+            return CurrentKeyBoardState.IsKeyDown(key) && LastKeyBoardState.IsKeyUp(key);
+        }
+
+        public bool IsKeyDown(Keys key) => CurrentKeyBoardState.IsKeyDown(key);
+        public bool IsNewKeyUp(Keys key)
+        {
+            return CurrentKeyBoardState.IsKeyUp(key) && LastKeyBoardState.IsKeyDown(key);
+        }
+    }
+}
