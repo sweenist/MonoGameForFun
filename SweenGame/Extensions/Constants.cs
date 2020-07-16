c5d189a7829195cb5abaa73191e96045022b63e5
Ryan Sweeney <sween119@hotmail.com>
2020-07-15 21:19:27 -0400

    Corrected Spelling in messages. Realigned MessageBox

--- SweenGame/Extensions/Constants.cs
+++ SweenGame/Extensions/Constants.cs
@@ -11,7 +11,7 @@ namespace SweenGame.Extensions
         public static int ScreenHeight = 624;
 
         public static string Current = "current";
-        public static string CancelMessage = "Are you sure yo want to quit this game?";
+        public static string CancelMessage = "Are you sure you want to quit this game?";
         public static string LoadingMessage = "Loading...";
         public static string CancelActionPrompt = "Press <ESC> to exit";
 
--- SweenGame/Screens/MessageBoxScreen.cs
+++ SweenGame/Screens/MessageBoxScreen.cs
@@ -6,8 +6,8 @@ namespace SweenGame.Screens
 {
     public class MessageBoxScreen : GameScreen
     {
-        private readonly string _usageText = "Press Enter or Space for Ok"
-                                             + $"{Environment.NewLine}Esc Cancels";
+        private readonly string _usageText = "Press <ENTER> or Space for Ok"
+                                             + $"{Environment.NewLine}<ESC> Cancels";
         private readonly string _message;
         private Texture2D _gradient;
 
@@ -41,16 +41,35 @@ namespace SweenGame.Screens
             var vPad = 16;
 
             var backgroundRect = new Rectangle((int)textPosition.X - hPad,
-                                               (int)textPosition.Y + vPad,
+                                               (int)textPosition.Y - vPad,
                                                (int)textSize.X + hPad * 2,
                                                (int)textSize.Y + vPad * 2);
+            var scale = GetScale(_gradient.Bounds.Size, backgroundRect.Size);
+
             var color = Color.White;
             color.A = TransitionAlpha;
 
             _screenManager.SpriteBatch.Begin();
-            _screenManager.SpriteBatch.Draw(_gradient, backgroundRect, color);
+            _screenManager.SpriteBatch.Draw(_gradient,
+                                            backgroundRect.Location.ToVector2(),
+                                            sourceRectangle: null,
+                                            color,
+                                            rotation: 0,
+                                            origin: Vector2.Zero,
+                                            scale,
+                                            SpriteEffects.None,
+                                            layerDepth: 0);
+
             _screenManager.SpriteBatch.DrawString(_screenManager.SpriteFont, _message, textPosition, color);
             _screenManager.SpriteBatch.End();
+        }
+
+        private Vector2 GetScale(Point source, Point target)
+        {
+            var horizontalScale = target.ToVector2().X / source.X;
+            var verticalScale = target.ToVector2().Y / source.Y;
+
+            return new Vector2(horizontalScale, verticalScale);
         }
 
         public override void HandleInput(Input.InputState state, GameTime gameTime)
